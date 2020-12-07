1243fb774006ec551a91ff799536925b094e8c9b
Rafee Jenkins <rafeejenkins@gmail.com>
2020-12-07 18:20:11 +0000

    part 2

--- d7/d7.py
+++ d7/d7.py
@@ -9,7 +9,7 @@ def process_bag(bag):
     formatted_bags = []
     for b in contains:
         if b == 'no other bags': 
-            return bag, [(0,0)]
+            return bag, {}
         else:
             c = b.strip().split(' ')
             formatted_bags.append((c[0],'{} {}'.format(c[1], c[2])))
@@ -23,14 +23,26 @@ def find_container_bags(bag, d):
     return bags
 
 def search_bags(target, d):
-    l = []
     if len(find_container_bags(target, d)) == 0:
         return target
     else:
+        l = []
         for i in find_container_bags(target, d):
             l.append(i)
             l.append(search_bags(i, d))
     return l
+
+def count_bags(root, d):
+    if d[root] == {}:
+        d[root] = 0
+    if isinstance(d[root], int):
+        return d[root] 
+    else:
+        res = 0
+        for i in d[root]:
+            res += d[root][i] * (1 + count_bags(i, d))
+        d[root] = res
+        return res
 
 d = {}
 for b in f:
@@ -38,5 +50,8 @@ for b in f:
     d[bag] = {i[1]:int(i[0]) for i in contain}  
 
 bags = search_bags('shiny gold', d)
-unique_bags = len({i:1 for i in list(more_itertools.collapse(bags))})
-print(unique_bags)
+unique_bags = {i:1 for i in list(more_itertools.collapse(bags))}
+print('Part 1 answer -> {}'.format(len(unique_bags)))
+
+bags_count = count_bags('shiny gold', d)
+print('Part 2 answer -> {}'.format(bags_count))
